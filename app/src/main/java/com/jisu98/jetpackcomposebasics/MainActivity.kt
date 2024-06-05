6cafc835092904d08eabd7b639de97cf0e3dbc87
jisu <jisu98@korea.ac.kr>
2024-06-06 00:13:12 +0900

    Replace remember with rememberSaveable to keep the state on recreation

--- app/src/main/java/com/jisu98/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/jisu98/jetpackcomposebasics/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -17,7 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,7 +40,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier = modifier) {
         if (shouldShowOnboarding) {
@@ -52,15 +54,15 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Android", "Jisu", "Minsu"),
+    names: List<String> = List(1000) { "$it" },
 ) {
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background,
     ) {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            names.forEach {
-                Greeting(it)
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+            items(names) { name ->
+                Greeting(name)
             }
         }
     }
@@ -68,7 +70,7 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
     val extraPadding = if (expanded) 24.dp else 0.dp
 
     Surface(
