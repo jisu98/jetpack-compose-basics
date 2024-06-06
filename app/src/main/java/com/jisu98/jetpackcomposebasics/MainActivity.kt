624473a7072c6cee3448c8d5ee3025d210133f37
jisu <jisu98@korea.ac.kr>
2024-06-06 14:01:32 +0900

    Refactor the code

--- app/src/main/java/com/jisu98/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/jisu98/jetpackcomposebasics/MainActivity.kt
@@ -7,15 +7,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,6 +31,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,35 +52,75 @@ class MainActivity : ComponentActivity() {
 fun MyApp(modifier: Modifier = Modifier) {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    Surface(modifier = modifier) {
+    Surface(modifier = modifier.background(MaterialTheme.colorScheme.background)) {
         if (shouldShowOnboarding) {
             OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
         } else {
-            Greetings()
+            GreetingCards()
         }
     }
 }
 
 @Composable
-fun Greetings(
+fun GreetingCards(
     modifier: Modifier = Modifier,
     names: List<String> = List(1000) { "$it" },
 ) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background,
+    LazyColumn(
+        modifier = modifier.padding(horizontal = 4.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-            items(names) { name ->
-                Greeting(name)
-            }
+        // top item decoration
+        item { Spacer(modifier = Modifier) }
+
+        items(names) { name ->
+            GreetingCard(name = name)
         }
+
+        // bottom item decoration
+        item { Spacer(modifier = Modifier) }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GreetingCard(modifier: Modifier = Modifier, name: String) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.primary,
+        ),
+    ) {
+        GreetingContents(name = name)
+    }
+}
+
+@Composable
+fun GreetingContents(
+    modifier: Modifier = Modifier,
+    name: String,
+) {
     var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Row(modifier = modifier.padding(24.dp)) {
+        GreetingText(
+            modifier = Modifier.weight(1f),
+            name = name,
+            expanded = expanded,
+        )
+        ShowMoreButton(
+            expanded = expanded,
+            onClick = { expanded = !expanded },
+        )
+    }
+}
+
+@Composable
+fun GreetingText(
+    modifier: Modifier = Modifier,
+    name: String,
+    expanded: Boolean,
+) {
     val extraPadding by animateDpAsState(
         targetValue = if (expanded) 24.dp else 0.dp,
         label = "extraPaddingDpAnimation",
@@ -85,40 +130,40 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         ),
     )
 
-    Surface(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp, horizontal = 8.dp),
-        color = MaterialTheme.colorScheme.primary,
+    Column(
+        modifier = modifier.padding(
+            bottom = extraPadding.coerceAtLeast(0.dp),
+        ),
     ) {
-        Row(
-            modifier = Modifier.padding(24.dp),
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp)),
-            ) {
-                Text(text = "Hello")
-                Text(
-                    style = MaterialTheme.typography.headlineMedium.copy(
-                        fontWeight = FontWeight.SemiBold,
-                    ),
-                    text = name,
-                )
-            }
-            ElevatedButton(
-                onClick = { expanded = !expanded },
-            ) {
-                Text(
-                    text = if (expanded) {
-                        "Show less"
-                    } else {
-                        "Show more"
-                    }
-                )
-            }
-        }
+        Text(text = stringResource(R.string.hello))
+        Text(
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.SemiBold,
+            ),
+            text = name,
+        )
+    }
+}
+
+@Composable
+fun ShowMoreButton(
+    modifier: Modifier = Modifier,
+    expanded: Boolean,
+    onClick: () -> Unit,
+) {
+    ElevatedButton(
+        modifier = modifier,
+        onClick = onClick,
+    ) {
+        Text(
+            text = stringResource(
+                if (expanded) {
+                    R.string.show_more
+                } else {
+                    R.string.show_less
+                }
+            )
+        )
     }
 }
 
@@ -126,13 +171,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     showBackground = true,
     widthDp = 320,
     uiMode = UI_MODE_NIGHT_YES,
-    name = "GreetingPreviewDark",
+    name = "GreetingCardsPreviewDark",
 )
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun GreetingPreview() {
+fun GreetingCardsPreview() {
     JetpackComposeBasicsTheme {
-        Greetings()
+        GreetingCards()
     }
 }
 
@@ -156,6 +201,13 @@ fun OnboardingScreen(
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "OnboardingPreviewDark",
+)
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
